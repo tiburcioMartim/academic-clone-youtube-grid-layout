92f3e0f4280f609958e837d5492e58f0d4e59975
Martim Tiburcio <tiburciomartim@gmail.com>
2024-06-25 08:02:34 -0300

    update menu js

--- assets/js/menu-trigger.js
+++ assets/js/menu-trigger.js
@@ -1,6 +1,8 @@
 const menuButton = document.querySelector('.menu-btn');
 const menuElement = document.querySelector('.menu-trigger');
+const mainContainer = document.getElementById('main-container')
 
 menuButton.addEventListener('click', () => {
-    menuElement.classList.toggle('open');
+    menuElement.classList.toggle('closed-menu');
+    mainContainer.classList.toggle('shrink-grid');
 });
